7952cb47894da1494f316659dfc360b0ff7d414e
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-25 21:00:29 +0300

    listeners for on element click events, important events

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,18 +4,36 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false,
+        important: false
+    };
+
     onLabelClick = () => {
-        console.log(`Done ${this.props.label}`);
+        this.setState({
+            done: !this.state.done
+        });
+    }
+
+    onMarkAsImportant = () => {
+        this.setState({
+            important: !this.state.important
+        });
     }
 
     render() {
 
-        const {important = false, label} = this.props;
+        const { label } = this.props;
+        const { done, important } = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+
+        if (important) {
+            classNames += ' important';
+        }
 
         const trashCanSVG = (
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash"
@@ -38,26 +56,26 @@ export default class TodoListItem extends Component {
         );
 
         return (
-            <span className="todo-list-item">
-            <span
-                className="todo-list-item-label"
-                style={style}
-                onClick={ this.onLabelClick }>
-                {label}
+            <span className={classNames}>
+                <span
+                    className="todo-list-item-label"
+                    onClick={ this.onLabelClick }>
+                    {label}
+                </span>
+                <span className="handling-button-box">
+                    <button
+                        type={"button"}
+                        className="btn btn-outline-danger btn-sm">
+                        {trashCanSVG}
+                    </button>
+                    <button
+                        type={"button"}
+                        className="btn btn-outline-success btn-sm"
+                        onClick={this.onMarkAsImportant}>
+                        {exclamationSGV}
+                    </button>
+                </span>
             </span>
-            <span className="handling-button-box">
-                <button
-                    type={"button"}
-                    className="btn btn-outline-danger btn-sm">
-                    {trashCanSVG}
-                </button>
-                <button
-                    type={"button"}
-                    className="btn btn-outline-success btn-sm">
-                    {exclamationSGV}
-                </button>
-            </span>
-        </span>
         );
     }
 
