0e70291b20ea89ab3d88575e170275682b72419d
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-25 21:50:30 +0300

    make App component class-based

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -7,26 +7,25 @@ import TodoList from "../todo-list";
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
+    render() {
+        const todoData = [
+            {label: 'Какая-то задача', important: false, id: 1},
+            {label: 'И еще задача', important: false, id: 2},
+            {label: 'И еще', important: true, id: 3}
+        ];
 
-    const todoData = [
-        {label: 'Какая-то задача', important: false, id: 1},
-        {label: 'И еще задача', important: false, id: 2},
-        {label: 'И еще', important: true, id: 3}
-    ];
-
-    return (
-        <div className="app">
-            <AppHeader toDo={1} done={3} />
-            <span className="input-group mb-3 d-flex">
+        return (
+            <div className="app">
+                <AppHeader toDo={1} done={3} />
+                <span className="input-group mb-3 d-flex">
                 <span><SearchPanel/></span>
                 <span><ItemStatusFilter/></span>
             </span>
-            <TodoList
-                todos={todoData}
-                onDeleted={ (id) => {console.log('del', id)} }/>
-        </div>
-    );
-};
-
-export default App;
+                <TodoList
+                    todos={todoData}
+                    onDeleted={ (id) => {console.log('del', id)} }/>
+            </div>
+        );
+    }
+}
