9f2bf59e9f1eab59452d624ede2503297ba8688b
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-26 14:32:54 +0300

    make component function-based

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
-    render() {
+const TodoListItem = (props) => {
 
-        const { label, onDeleted, done, important, onToggleDone, onToggleImportant } = this.props;
+    const { label, onDeleted, done, important, onToggleDone, onToggleImportant } = props;
 
-        let classNames = 'todo-list-item';
-        if (done) {
-            classNames += ' done';
-        }
+    let classNames = 'todo-list-item';
+    if (done) {
+        classNames += ' done';
+    }
 
-        if (important) {
-            classNames += ' important';
-        }
+    if (important) {
+        classNames += ' important';
+    }
 
-        const trashCanSVG = (
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash"
-                 viewBox="0 0 16 16">
-                <path
-                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0
+    const trashCanSVG = (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash"
+             viewBox="0 0 16 16">
+            <path
+                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0
                 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                <path
-                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1
+            <path
+                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1
                 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0
                 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-            </svg>
-        );
+        </svg>
+    );
 
-        const exclamationSGV = (
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                 className="bi bi-exclamation-lg" viewBox="0 0 16 16">
-                <path d="M7.005 3.1a1 1 0 1 1 1.99 0l-.388 6.35a.61.61 0 0 1-1.214 0zM7 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0"/>
-            </svg>
-        );
+    const exclamationSGV = (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+             className="bi bi-exclamation-lg" viewBox="0 0 16 16">
+            <path d="M7.005 3.1a1 1 0 1 1 1.99 0l-.388 6.35a.61.61 0 0 1-1.214 0zM7 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0"/>
+        </svg>
+    );
 
-        return (
-            <span className={classNames}>
+    return (
+        <span className={classNames}>
                 <span
                     className="todo-list-item-label"
                     onClick={ onToggleDone }>
@@ -58,7 +57,8 @@ export default class TodoListItem extends Component {
                     </button>
                 </span>
             </span>
-        );
-    }
+    );
 
 }
+
+export default TodoListItem;
