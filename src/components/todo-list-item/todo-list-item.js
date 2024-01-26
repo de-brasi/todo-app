5da7bab4f18ae3eb1f73bb3da8f825b93ba8d3e1
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-26 14:30:16 +0300

    make stateless; Now no visible changes when marks as done or important

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,38 +3,9 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-
-    state = {
-        done: false,
-        important: false
-    };
-
-    onLabelClick = () => {
-        // callback to App
-        this.props.onToggleDone();
-
-        this.setState((state) => {
-            return {
-                done: !state.done
-            };
-        });
-    }
-
-    onMarkAsImportant = () => {
-        // callback to App
-        this.props.onToggleImportant();
-
-        this.setState((state) => {
-            return {
-                important: !state.important
-            };
-        });
-    }
-
     render() {
 
-        const { label, onDeleted, onToggleDone, onToggleImportant } = this.props;
-        const { done, important } = this.state;
+        const { label, onDeleted, done, important, onToggleDone, onToggleImportant } = this.props;
 
         let classNames = 'todo-list-item';
         if (done) {
@@ -69,20 +40,20 @@ export default class TodoListItem extends Component {
             <span className={classNames}>
                 <span
                     className="todo-list-item-label"
-                    onClick={ this.onLabelClick }>
+                    onClick={ onToggleDone }>
                     {label}
                 </span>
                 <span className="handling-button-box">
                     <button
-                        type={"button"}
+                        type="button"
                         className="btn btn-outline-danger btn-sm"
-                        onClick={onDeleted}>
+                        onClick={ onDeleted }>
                         {trashCanSVG}
                     </button>
                     <button
-                        type={"button"}
+                        type="button"
                         className="btn btn-outline-success btn-sm"
-                        onClick={this.onMarkAsImportant}>
+                        onClick={ onToggleImportant }>
                         {exclamationSGV}
                     </button>
                 </span>
