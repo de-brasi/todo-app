ecc3ae890b5554da2c3accb701011de12dc30c60
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-26 13:24:54 +0300

    add form for new task input

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import './add-item.css';
+
 const AddItem = () => {
-    return (<button type="button">Add task</button>);
+    return (
+        <span className="form-group mb-3 d-flex">
+            <input type="text"
+                   className="form-control task-input"
+                    placeholder="task's description">
+            </input>
+            <div className="btn-group">
+                <button
+                    type="button"
+                    className="btn btn-success">
+                    Add task
+                </button>
+            </div>
+        </span>
+    );
 };
 
 export default AddItem;
