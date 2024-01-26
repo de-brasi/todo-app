b06e9645c89b86dc4a7ca07083d9dbcc5049ccb8
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-27 00:04:30 +0300

    adding a new task with text

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './item-add-form.css';
 
-const ItemAddForm = ({ onAdd }) => {
-    return (
-        <form className="item-add-form form-group mb-3 d-flex">
-            <input type="text"
-                   className="form-control task-input"
-                   placeholder="task's description">
-            </input>
-            <div className="btn-group">
+export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.label);
+        this.setState({label: ''});
+    }
+
+    render() {
+        return (
+            <form className="item-add-form form-group mb-3 d-flex"
+                  onSubmit={this.onSubmit}>
+                <input type="text"
+                       className="form-control task-input"
+                       placeholder="task's description"
+                       onChange={this.onLabelChange}
+                       value={this.state.label}/>
                 <button
-                    type="button"
-                    className="btn btn-success"
-                    onClick={ () => onAdd('new task') }>
+                    className="btn btn-success">
                     Add task
                 </button>
-            </div>
-        </form>
-    );
-};
+            </form>
+        );
+    }
 
-export default ItemAddForm;
+}
