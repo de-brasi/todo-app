4888cbd589acfb39c93f59c28bcccc927d392840
de_brasi <ya.kakadu.ru@yandex.ru>
2024-01-30 19:18:55 +0300

    switch button's CSS class according chosen button

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -2,25 +2,74 @@ import React, { Component } from 'react';
 
 import './item-status-filter.css';
 
+const ButtonTags = {
+    ALL: 'all',
+    ACTIVE: 'active',
+    DONE: 'done'
+};
+
+const ButtonClasses = {
+    CHOSEN: 'btn btn-primary',
+    DEFAULT: 'btn btn-outline-secondary'
+};
+
 export default class ItemStatusFilter extends Component {
+
+    state = {
+        activeButton: ButtonTags.ALL
+    };
+
+    onButtonAllClick = () => {
+        this.setState(
+            {activeButton: ButtonTags.ALL}
+        );
+        this.props.onFilterChange(ButtonTags.ALL)
+    };
+
+    onButtonActiveClick = () => {
+        this.setState(
+            {activeButton: ButtonTags.ACTIVE}
+        );
+        this.props.onFilterChange(ButtonTags.ACTIVE)
+    };
+
+    onButtonDoneClick = () => {
+        this.setState(
+            {activeButton: ButtonTags.DONE}
+        );
+        this.props.onFilterChange(ButtonTags.DONE)
+    };
+
     render() {
 
-        const { onFilterChange } = this.props;
+        const buttonsClassNames = {
+            [ButtonTags.ALL]: (
+                this.state.activeButton === ButtonTags.ALL ? ButtonClasses.CHOSEN : ButtonClasses.DEFAULT
+            ),
+            [ButtonTags.ACTIVE]: (
+                this.state.activeButton === ButtonTags.ACTIVE ? ButtonClasses.CHOSEN : ButtonClasses.DEFAULT
+            ),
+            [ButtonTags.DONE]: (
+                this.state.activeButton === ButtonTags.DONE ? ButtonClasses.CHOSEN : ButtonClasses.DEFAULT
+            ),
+        };
+
+        console.log(buttonsClassNames)
 
         return (
             <div className="btn-group">
                 <button
                     type="button"
-                    className="btn btn-primary"
-                    onClick={ ()=>{onFilterChange('all')} }>All</button>
+                    className={ buttonsClassNames[ButtonTags.ALL] }
+                    onClick={ this.onButtonAllClick }>All</button>
                 <button
                     type="button"
-                    className="btn btn-outline-secondary"
-                    onClick={ ()=>{onFilterChange('active')} }>Active</button>
+                    className={ buttonsClassNames[ButtonTags.ACTIVE] }
+                    onClick={ this.onButtonActiveClick }>Active</button>
                 <button
                     type="button"
-                    className="btn btn-outline-secondary"
-                    onClick={ ()=>{onFilterChange('done')} }>Done</button>
+                    className={ buttonsClassNames[ButtonTags.DONE] }
+                    onClick={ this.onButtonDoneClick }>Done</button>
             </div>
         );
     }
